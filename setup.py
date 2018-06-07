dcacd825b18fcd61815aa46de40f45bd833f6416
Xuesong Hu <xuesong@pit.ai>
2018-06-07 15:21:30 -0700

    Comment on changes made.

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ import os
 here = dirname(__file__)
 setup(
     name='ledgerblue',
+	# Bump up the version from 0.1.17 to 0.1.18alpha
+	# by Xuesong Hu @ Pit.AI
     version='0.1.18alpha',
     author='Ledger',
     author_email='hello@ledger.fr',
@@ -15,6 +17,8 @@ setup(
     long_description=open(join(here, 'README.md')).read(),
     url='https://github.com/LedgerHQ/blue-loader-python',
     packages=find_packages(),
+	# Change one required package from pycrypto>=2.6.1 to pycryptodome>=3.6.1
+	# by Xuesong Hu @ Pit.AI
     install_requires=['hidapi>=0.7.99', 'protobuf>=2.6.1', 'pycryptodome>=3.6.1', 'future', 'ecpy>=0.8.1', 'pillow>=3.4.0', 'python-u2flib-host>=3.0.2'],
     extras_require = {
 	'smartcard': [ 'python-pyscard>=1.6.12-4build1' ]
